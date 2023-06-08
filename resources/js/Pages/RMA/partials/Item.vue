357399e405068122ed66a35c986b8c37333b3c08
Stephen <sd>
2023-06-08 17:37:31 +0100

    can add item component and remove it

---
+++ resources/js/Pages/RMA/partials/Item.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li v-for="subItem in items" :key="subItem.value">
+
+            <InputLabel value="type"/>
+            <InputError/>
+            <TextInput modelValue=""/>
+
+            {{ subItem.text }}
+            {{ subItem.value }}
+        </li>
+    </ul>
+</template>
+
+<script>
+import InputLabel from "../../../Components/InputLabel.vue";
+import InputError from "../../../Components/InputError.vue";
+import TextInput from "../../../Components/TextInput.vue";
+
+export default {
+    components: {TextInput, InputError, InputLabel},
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    name: 'RMAItem'
+};
+</script>
